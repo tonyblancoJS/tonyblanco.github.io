42b4ecfcfd3a47bf61c8a47afcaeab94d65f7df0
Antonio Blanco <115665302+tonyblancoJS@users.noreply.github.com>
2023-01-24 14:23:39 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,32 @@
+document.getElementById("inicio").addEventListener('mousedown', function(){
+    document.getElementById('inicio').style.background ='black';
+});
+document.getElementById("inicio").addEventListener('mouseup', function(){
+    document.getElementById('inicio').style.background ='white';
+});
+
+document.getElementById("laboral").addEventListener('mousedown', function(){
+    document.getElementById('laboral').style.background ='black';
+});
+document.getElementById("laboral").addEventListener('mouseup', function(){
+    document.getElementById('laboral').style.background ='white';
+});
+document.getElementById("estudio").addEventListener('mousedown', function(){
+    document.getElementById('estudio').style.background ='black';
+});
+document.getElementById("estudio").addEventListener('mouseup', function(){
+    document.getElementById('estudio').style.background ='white';
+
+});document.getElementById("idioma").addEventListener('mousedown', function(){
+    document.getElementById('idioma').style.background ='black';
+});
+document.getElementById("idioma").addEventListener('mouseup', function(){
+    document.getElementById('idioma').style.background ='white';
+
+});document.getElementById("otro").addEventListener('mousedown', function(){
+    document.getElementById('otro').style.background ='black';
+});
+document.getElementById("otro").addEventListener('mouseup', function(){
+    document.getElementById('otro').style.background ='white';
+});
+
