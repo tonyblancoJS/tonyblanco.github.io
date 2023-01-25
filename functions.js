c5c0b6c95d42d5ab91d25c7fa665aa77a291d847
Antonio Blanco <115665302+tonyblancoJS@users.noreply.github.com>
2023-01-25 19:00:33 -0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -4,11 +4,16 @@ document.getElementById("inicio").addEventListener('mousedown', function(){
 document.getElementById("inicio").addEventListener('mouseup', function(){
     document.getElementById('inicio').style.background ='white';
 });
-
+document.getElementById("inicio").addEventListener('mouseleave', function(){
+        document.getElementById('inicio').style.background ='white';
+});
 document.getElementById("laboral").addEventListener('mousedown', function(){
     document.getElementById('laboral').style.background ='black';
 });
 document.getElementById("laboral").addEventListener('mouseup', function(){
+    document.getElementById('laboral').style.background ='white';
+});
+document.getElementById("laboral").addEventListener('mouseleave', function(){
     document.getElementById('laboral').style.background ='white';
 });
 document.getElementById("estudio").addEventListener('mousedown', function(){
@@ -16,17 +21,71 @@ document.getElementById("estudio").addEventListener('mousedown', function(){
 });
 document.getElementById("estudio").addEventListener('mouseup', function(){
     document.getElementById('estudio').style.background ='white';
-
-});document.getElementById("idioma").addEventListener('mousedown', function(){
+});
+document.getElementById("estudio").addEventListener('mouseleave', function(){
+     document.getElementById('estudio').style.background ='white';
+});
+document.getElementById("idioma").addEventListener('mousedown', function(){
     document.getElementById('idioma').style.background ='black';
 });
 document.getElementById("idioma").addEventListener('mouseup', function(){
     document.getElementById('idioma').style.background ='white';
-
-});document.getElementById("otro").addEventListener('mousedown', function(){
+});
+document.getElementById("idioma").addEventListener('mouseleave', function(){
+    document.getElementById('idioma').style.background ='white';
+});
+document.getElementById("otro").addEventListener('mousedown', function(){
     document.getElementById('otro').style.background ='black';
 });
 document.getElementById("otro").addEventListener('mouseup', function(){
     document.getElementById('otro').style.background ='white';
 });
+document.getElementById("otro").addEventListener('mouseleave', function(){
+    document.getElementById('otro').style.background ='white';
+});
+document.getElementById("icon-domicilio").addEventListener('mouseover', function(){
+    document.getElementById('icon-domicilio').style.width ='22px';
+});
+document.getElementById("icon-domicilio").addEventListener('mouseout', function(){
+    document.getElementById('icon-domicilio').style.width ='20px';
+});
+document.getElementById("icon-mobile").addEventListener('mouseover', function(){
+    document.getElementById('icon-mobile').style.width ='22px';
+});
+document.getElementById("icon-mobile").addEventListener('mouseout', function(){
+    document.getElementById('icon-mobile').style.width ='20px';
+});
+document.getElementById("icon-mail").addEventListener('mouseover', function(){
+    document.getElementById('icon-mail').style.width ='22px';
+});
+document.getElementById("icon-mail").addEventListener('mouseout', function(){
+    document.getElementById('icon-mail').style.width ='20px';
+});
+document.getElementById("icon-escudo").addEventListener('mouseover', function(){
+    document.getElementById("icon-escudo").style.width ='32px';
+});
+document.getElementById("icon-escudo").addEventListener('mouseout', function(){
+    document.getElementById("icon-escudo").style.width ='30px';
+
+});document.getElementById("icon-utn").addEventListener('mouseover', function(){
+    document.getElementById('icon-utn').style.width ='32px';
+});
+document.getElementById("icon-utn").addEventListener('mouseout', function(){
+    document.getElementById('icon-utn').style.width ='30px';
+
+});document.getElementById("icon-conf-suiza").addEventListener('mouseover', function(){
+    document.getElementById("icon-conf-suiza").style.width ='32px';
+});
+document.getElementById("icon-conf-suiza").addEventListener('mouseout', function(){
+    document.getElementById("icon-conf-suiza").style.width ='30px';
+});
+
+document.getElementById("icon-cui").addEventListener('mouseover', function(){
+    document.getElementById("icon-cui").style.width ='32px';
+});
+document.getElementById("icon-cui").addEventListener('mouseout', function(){
+    document.getElementById("icon-cui").style.width ='30px';
+});
+
+
 
